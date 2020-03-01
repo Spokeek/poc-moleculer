d32d428a49d6bc5b59b172f62729c104a3d1919f
Spokeek <spokeek@protonmail.com>
2020-03-01 02:26:54 +0100

    ajout du front

---
+++ app/front/src/profile/Profil.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    props: {
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h2>Profil</h2>
+        <ul>
+            <li>Email: {{email}}</li>
+            <li>Prénom: {{prenom}}</li>
+            <li>Nom: {{nom}}</li>
+        </ul>
+    </div>
+</template>
