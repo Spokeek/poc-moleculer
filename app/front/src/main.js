153f4eb7d3857f21cafe45a8662ddbeeac8cbfb4
Spokeek <spokeek@protonmail.com>
2020-03-01 04:03:51 +0100

    ajout de vuelidate

--- app/front/src/main.js
+++ app/front/src/main.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
+import Vuelidate from 'vuelidate'
 //import { createProvider } from "./vue-apollo";
 import router from "./router";
 
 import Root from "./Root.vue";
 import vuetify from "./plugins/vuetify";
-
+Vue.use(Vuelidate);
 Vue.config.productionTip = false;
 
 new Vue({
